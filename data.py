e58b7d662ae9f425bd033d6229b785eade3a08a2
Phung Huynh Xuan <phunghx2@fpt.com.vn>
2021-09-11 07:26:48 +0700

    data

---
+++ data.py
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+int euclid(int m[MSSV], int n[MSSV])
+{
+    //////////you code here ////////
+
+    //////////end your code/////////
+    return n[MSSV];
+}
+void main()
+{
+    std::cout << euclid(27,15);
+    //Result=3
+}
